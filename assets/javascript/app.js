b984209fdc8299b6088283b013043b1f902f1f86
Dhruv Patel <darup67@gmail.com>
2019-01-09 23:08:58 -0500

    Firebase-Assignment---Train-Scheduler

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,15 @@
 $(document).ready(function() {
     var config = {
-        apiKey: "AIzaSyBPCAxjycmuYaeY8RISjpPzUOSfmZIbQrQ",
-        authDomain: "train-scheduler-68643.firebaseapp.com",
-        databaseURL: "https://train-scheduler-68643.firebaseio.com",
-        projectId: "train-scheduler-68643",
-        storageBucket: "train-scheduler-68643.appspot.com",
-        messagingSenderId: "934455543386"
+        apiKey: "AIzaSyA4ytXPFchzTpXeFYCH0Yj-RAmLJ09Gsc8",
+        authDomain: "train-schedule-a2ea9.firebaseapp.com",
+        databaseURL: "https://train-schedule-a2ea9.firebaseio.com",
+        projectId: "train-schedule-a2ea9",
+        storageBucket: "train-schedule-a2ea9.appspot.com",
+        messagingSenderId: "623782715835"
     };
     firebase.initializeApp(config);
 
-
     var database = firebase.database();
-
 
     var name;
     var destination;
@@ -20,12 +18,10 @@ $(document).ready(function() {
 
     $("#add-train").on("click", function() {
         event.preventDefault();
-
         name = $("#train-name").val().trim();
         destination = $("#destination").val().trim();
         firstTrain = $("#first-train").val().trim();
         frequency = $("#frequency").val().trim();
-
 
         database.ref().push({
             name: name,
@@ -46,19 +42,18 @@ $(document).ready(function() {
         var minAway = childSnapshot.val().frequency - remainder;
         var nextTrain = moment().add(minAway, "minutes");
         nextTrain = moment(nextTrain).format("hh:mm");
+
         $("#add-row").append("<tr><td>" + childSnapshot.val().name +
             "</td><td>" + childSnapshot.val().destination +
             "</td><td>" + childSnapshot.val().frequency +
             "</td><td>" + nextTrain +
             "</td><td>" + minAway + "</td></tr>");
 
-
     }, function(errorObject) {
         console.log("Errors handled: " + errorObject.code);
     });
 
     database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
-
         $("#name-display").html(snapshot.val().name);
         $("#email-display").html(snapshot.val().email);
         $("#age-display").html(snapshot.val().age);
